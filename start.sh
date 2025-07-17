a79e2ae9aaec63939b8d7547c2be43668f6d9262
Bhavya Sharma <your_email@example.com>
2025-07-18 02:15:33 +0530

    Make start.sh executable

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
-apt update && apt install ffmpeg -y
+#!/bin/bash
+
+# Add ffmpeg folder to PATH
+export PATH=$PWD/bin:$PATH
+
+# Show version (just for confirmation)
+ffmpeg -version
+
+# Start the Python app
 python app.py
+
+
