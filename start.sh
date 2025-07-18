174b1377b0c98f7d1a37597c16110d8004d54d9f
Bhavya sharma <umakantsharma6981@gmail.com>
2025-07-19 00:26:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
 # Step 1: Update and install FFmpeg
 apt update && apt install ffmpeg -y
-
+apt-get update && apt-get install -y ffmpeg
+pip install -U yt-dlp
 # Step 2: Start your app
 python app.py
