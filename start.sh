6c6a1d7abe4c4d7abe4269807214ddb2047eb861
Bhavya sharma <umakantsharma6981@gmail.com>
2025-07-20 12:21:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,19 +6,16 @@ set -e
 # Step 1: Install dependencies
 echo "[*] Installing dependencies..."
 apt update
-apt install -y torsocks ffmpeg tor python3-pip
+apt install -y  ffmpeg  python3-pip
 
 # Step 2: Upgrade yt-dlp
 echo "[*] Installing yt-dlp..."
 pip install -U yt-dlp
 
-# Step 3: Start Tor in the background
-echo "[*] Starting Tor..."
-tor &
 
-# Wait for Tor to initialize
-sleep 10
+
+
 
 # Step 4: Launch your Python app via torsocks
 echo "[*] Starting your app..."
-torsocks python3 app.py
+python3 app.py
